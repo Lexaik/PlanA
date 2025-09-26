44d4a1cb5e6d4b46135e2d4addc01e621b7c7332
lexaik <lexaik@live.com>
2025-09-26 12:21:00 +0400

    что-то

---
+++ Models/OperationComponents.cs
@@ -0,0 +1,9 @@
+namespace PlanA.Models;
+
+public class OperationComponents
+{
+    public int Id { get; set; }
+    public int Order_Id { get; set; }
+    public int Item_Id { get; set; }
+    public int Quantity { get; set; }
+}
