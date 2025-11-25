f81cd7799d8c72210e8cbd89ee4d18edb711e692
lexaik <lexaik@live.com>
2025-11-25 13:03:41 +0400

    начал делать фронт

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,13 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using PlanA.Context;
 using PlanA.Models;
 
 namespace PlanA.Controllers;
 
 public class HomeController : Controller {
     private readonly ILogger<HomeController> _logger;
-
+    
     public HomeController(ILogger<HomeController> logger) {
         _logger = logger;
     }
---
+++ Controllers/WorkController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlanA.Context;
+using PlanA.Models;
+
+namespace PlanA.Controllers;
+
+public class WorkController : Controller {
+    private readonly ILogger<WorkController> _logger;
+
+    PlanADbContext db;
+    public WorkController(PlanADbContext context, ILogger<WorkController> logger, PlanADbContext db) {
+        db = context;
+        _logger = logger;
+        this.db = db;
+    }
+    public WorkController(ILogger<WorkController> logger, PlanADbContext db) {
+        _logger = logger;
+        this.db = db;
+    }
+
+    public async Task<IActionResult> Index() {
+        return View(await db.Items.ToListAsync());
+    }
+
+    public IActionResult Create() {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(Item item)
+    {
+        db.Items.Add(item);
+        await db.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+    
+}
