bfaf8e433a79fce191d6bf753c054fa602021b8f
Алексей <lexaik@live.com>
2026-04-07 13:23:26 +0400

    ok

--- ModelConfigurations/OrderConfiguration.cs
+++ ModelConfigurations/OrderConfiguration.cs
@@ -15,35 +15,22 @@ public class OrderConfiguration : IEntityTypeConfiguration<Order> {
             .IsRequired()
             .HasColumnType("datetime2");
         builder.Ignore(o => o.OrderItems);
-        builder.OwnsMany(o => o._items, ownedBuilder =>
-        {
+        builder.OwnsMany(o => o.OrderItems, ownedBuilder => {
             ownedBuilder.ToTable("OrderItems");
-            
-            // Теневой первичный ключ с автоинкрементом
             ownedBuilder.Property<int>("Id")
                 .ValueGeneratedOnAdd()
                 .HasColumnType("int");
-            
             ownedBuilder.HasKey("Id");
-            
-            // Внешний ключ
             ownedBuilder.WithOwner()
                 .HasForeignKey("OrderId");
-            
-            // Настройка свойств
-            ownedBuilder.Property(i => i.Name)
+            ownedBuilder.Property(i => i.ItemId)
                 .IsRequired()
                 .HasMaxLength(200)
                 .HasColumnName("ItemName");
-            
             ownedBuilder.Property(i => i.Quantity)
                 .IsRequired()
                 .HasColumnName("Quantity");
-            
-            // Индекс для ускорения запросов
             ownedBuilder.HasIndex("OrderId");
-            
-            // Уникальный индекс для предотвращения дубликатов
             ownedBuilder.HasIndex("OrderId", "Name")
                 .IsUnique();
         });
